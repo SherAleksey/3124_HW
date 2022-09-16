3a6c78fb2b124779d84a4bd1eee01bc87ca50d2c
Aleksey <201920@mail.ru>
2022-09-16 19:46:53 +0400

    realized all seminar task

---
+++ seminar2/task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Random rand = new Random();
+int number = rand.Next(100, 1000);
+Console.WriteLine($"Сгенерировалось случайное число {number}");
+int digit2 = (number % 100)/10;
+int res = digit2;
+Console.WriteLine(res);
---
+++ seminar2/task13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number > 1000)
+{
+    number = number / 10;
+}
+if (number > 99)
+{
+    int digit = number % 10;
+    int result = digit % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет ");
+}
