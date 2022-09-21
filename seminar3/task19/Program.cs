9263f53692e5188e43cb365c913bf3e335aecdcf
Aleksey <201920@mail.ru>
2022-09-21 12:13:32 +0400

    realized all seminar task

---
+++ seminar3/task19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+	int number = Convert.ToInt32(Console.ReadLine());
+	string num = Convert.ToString(number);
+	    if (num.Length == 5)
+	    {
+	        if(num[0] == num[4] && (num[1] == num[3]))
+	    
+	        Console.WriteLine("ПАЛИНДРОМ" );
+	    
+	        else 
+	    
+	        Console.WriteLine("НЕ ПАЛИНДРОМ" );
+	    
+	    }
+	
+	     else    
+	{
+	    Console.WriteLine("!не пятизначное число!" );
+	}
---
+++ seminar3/task23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, 
+// которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+	int number = Convert.ToInt32(Console.ReadLine());
+	if (number > 0)
+	{
+	    for (int i = 1; i <= number; i++)
+	    {
+	        Console.Write(i * i * i + " ");
+	    }
+	
+	}
+	
+	else
+	{
+	   Console.WriteLine("number ДОЛЖНО БЫТЬ БОЛЬШЕ 0" ); 
+	}
