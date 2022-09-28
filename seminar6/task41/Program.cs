1e62f5ffbec020ba6c4d3d04a52a2289c23a5350
Aleksey <201920@mail.ru>
2022-09-28 11:36:39 +0400

    realized all seminar task

---
+++ seminar6/task41/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//  Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа, для подсчёта чисел > 0, введите stop");
+int countPositive = 0;
+while (true)
+{
+    string answer = Console.ReadLine() ?? "";
+    
+    if (answer == "stop")
+    {
+        break;
+    }
+    else
+    {
+        int.TryParse(answer, out int answerNumber);
+        if (answerNumber > 0)
+        {
+    
+            countPositive++;
+        }
+    }
+}
+
+Console.WriteLine($"КОЛИЧЕСТВО ЧИСЕЛ > 0: {countPositive} ");
