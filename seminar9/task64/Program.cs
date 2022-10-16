fd8d722b1f1b5fdf931ec165501c3c94b43e4b0f
Aleksey <201920@mail.ru>
2022-10-16 14:22:58 +0400

    realized all seminar task

---
+++ seminar9/task64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNto1(int n, int count = 1) 
+{
+    if(count > n)
+    {
+        return;
+    }
+    else
+    {
+        PrintNto1(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
+
+int number = ReadNumber("Введите N");
+PrintNto1(number);
---
+++ seminar9/task66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintSumMtoN(int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + m++;
+    PrintSumMtoN(m, n, sum);
+}
+
+int m = ReadNumber("Введите M");
+int n = ReadNumber("Введите N");
+
+PrintSumMtoN(m, n, 0);
